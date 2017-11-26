f658628dd0132dc4a5e63c2aca1b0454e117cf94
Harry Yu <hy.harry.yu@gmail.com>
2017-11-25 18:14:31 -0800

    Added script to run model.

---
+++ research/object_detection/run_model.py
@@ -0,0 +1,161 @@
+# Sample usage:
+# python models/research/object_detection/run_model.py  --model runs\1\model\frozen_inference_graph.pb --labels runs\1\data\costco_label_map.pbtxt --box-output temp\boxes --output temp\output temp\input
+
+import argparse
+import json
+import os
+import re
+
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+def load_graph(model_file):
+  detection_graph = tf.Graph()
+  with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(model_file, 'rb') as fid:
+      serialized_graph = fid.read()
+      od_graph_def.ParseFromString(serialized_graph)
+      tf.import_graph_def(od_graph_def, name='')
+
+  return detection_graph
+
+def load_label_map_category_index(labels_file):
+  with open(labels_file, 'r') as opened_file:
+    label_contents = opened_file.read()
+    num_classes = len(re.findall('id:', label_contents))
+
+  label_map = label_map_util.load_labelmap(labels_file)
+  categories = label_map_util.convert_label_map_to_categories(
+      label_map, max_num_classes=num_classes, use_display_name=True)
+  category_index = label_map_util.create_category_index(categories)
+
+  return category_index
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+# Converts numpy-format box to PIL format box
+def pil_box_from_np_box(np_box, im_width, im_height):
+  (ymin, xmin, ymax, xmax) = np_box
+  # For reference, box = (left, upper, right, lower)
+  return (xmin * im_width, ymin * im_height, xmax * im_width, ymax * im_height)
+
+def run_graph_on_images(image_paths, detection_graph, category_index, min_score, image_dir=None):
+  with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+      # Definite input and output Tensors for detection_graph
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+      images_objects = {}
+
+      for image_path in image_paths:
+        image = Image.open(image_path)
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        image_np = load_image_into_numpy_array(image)
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+
+        # Run the model!
+        (boxes, scores, classes, _) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+
+        # Write images if we have a directory
+        if image_dir is not None:
+          vis_util.visualize_boxes_and_labels_on_image_array(
+              image_np, # Modified as output
+              np.squeeze(boxes),
+              np.squeeze(classes).astype(np.int32),
+              np.squeeze(scores),
+              category_index,
+              use_normalized_coordinates=True,
+              line_thickness=8)
+          writeable_image = Image.fromarray(image_np, 'RGB')
+          file_name = os.path.basename(image_path)
+          writeable_image.save(os.path.join(image_dir, file_name + '.jpg'))
+
+        # Get the objects into a encodable file
+        image_objects = []
+        (im_width, im_height) = image.size
+        for i, score in enumerate(scores[0]):
+          if score > min_score:
+            native_score = np.asscalar(score)
+            np_box = [np.asscalar(coord) for coord in boxes[0][i]]
+            pil_box = pil_box_from_np_box(np_box, im_width, im_height)
+            image_objects.append({'score': native_score, 'box': pil_box})
+        images_objects[image_path] = image_objects
+
+      return images_objects
+
+def get_images_in_dir(image_dir):
+  files = os.listdir(image_dir)
+  files = [file_name for file_name in files if '.jpg' in file_name or '.jpeg' in file_name]
+  return [os.path.join(image_dir, file_name) for file_name in files]
+
+def clamp(num, min_num, max_num):
+  return min(max(num, min_num), max_num)
+
+def crop_image_to_boxes(image_path, identified_objects, cropped_output_dir, buffer_ratio=0.10):
+  file_name = os.path.basename(image_path)
+  image = Image.open(image_path)
+  (im_width, im_height) = image.size
+
+  for i, object_info in enumerate(identified_objects):
+    (left, top, right, bottom) = object_info['box']
+
+    # Add a bit to width/height depending on buffer ratio
+    crop_width = right - left
+    crop_height = bottom - top
+    extra_width_per_side = crop_width * (buffer_ratio / 2)
+    extra_height_per_side = crop_height * (buffer_ratio / 2)
+    left = left - extra_width_per_side
+    right = right + extra_width_per_side
+    top = top - extra_height_per_side
+    bottom = bottom + extra_height_per_side
+    box = [
+        clamp(left, 0, im_width),
+        clamp(top, 0, im_height),
+        clamp(right, 0, im_width),
+        clamp(bottom, 0, im_height)
+    ]
+    box = [round(coord) for coord in box]
+    new_image = image.crop(box)
+    new_image.save(os.path.join(cropped_output_dir, '{}.{}.jpg'.format(file_name, i)))
+
+def main():
+  parser = argparse.ArgumentParser(description='Runs a tensorflow model against a directory of images')
+  parser.add_argument('--model', dest='model_file', required=True, help='Usually a frozen_inference_graph.pb')
+  parser.add_argument('--labels', dest='labels_file', required=True, help='A label-map pbtxt')
+  parser.add_argument('--output', dest='cropped_output_dir', required=True, help='Where to output cropped images')
+  parser.add_argument('--box-output', dest='box_output_dir', default=None, help='Directory to put files with outline boxes')
+  parser.add_argument('--min-score', dest='min_score', type=float, default=0.5, help='Default 0.5, Minimum score to record boxes over')
+  parser.add_argument('dir', help='Directory with images')
+
+  args = parser.parse_args()
+
+  detection_graph = load_graph(args.model_file)
+  category_index = load_label_map_category_index(args.labels_file)
+  image_paths = get_images_in_dir(args.dir)
+
+  images_objects = run_graph_on_images(image_paths, detection_graph, category_index, args.min_score, args.box_output_dir)
+  print(json.dumps(images_objects))
+  for image_path, objects in images_objects.items():
+    crop_image_to_boxes(image_path, objects, args.cropped_output_dir)
+
+if __name__ == '__main__':
+  main()
